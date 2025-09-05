f7994c411e3df50056d6b8dbdc97b39a199ab4fc
LilyW3865 <li733414@ucf.edu>
2025-09-05 14:52:08 -0400

    Updated Enemy Spawner

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -5,41 +5,21 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    /* [System.Serializable]
-     public class EnemyType
-     {
-         public string type;
-         public GameObject enemy;
-     }*/
+    private int randomEnemy;
+    public List<GameObject> enemy;
 
-    // public GameObject enemy;
-   
-   public List<GameObject> enemy;
-
-    [SerializeField] private int poolSize;
-    private Dictionary<string, GameObject> enemyPrefabs = new Dictionary<string, GameObject>();
+   // [SerializeField] private int poolSize;
+   // private Dictionary<string, GameObject> enemyPrefabs = new Dictionary<string, GameObject>();
     void Start()
     {
-       /* foreach (EnemyType enemyType in enemyTypes)
-        {
-            EnemyObjectPool.Instance.CreatePool(enemyType.type, enemyType.enemy, enemyType.poolSize);
-            enemyPrefabs.Add(enemyType.type, enemyType.enemy);
-        }*/
         SpawnEnemy(3);
-       
     }
     public void SpawnEnemy(int count)
     {
-        List<string> availableEnemies = new List<string>(enemyPrefabs.Keys);
         for (int i = 0; i < 3; i++)
         {
-            int randomEnemyIndex = Random.Range(0, availableEnemies.Count);
-           // string randomEnemyTag = availableEnemies[randomEnemyIndex];
-
-            //Spawn Point code
-
-           // GameObject enemy2 = EnemyObjectPool.Instance.GetPooledObject(enemyPrefabs);
+            randomEnemy = Random.Range(0, enemy.Count);
+            Instantiate(enemy[randomEnemy]);
         }
-
     }
 }
