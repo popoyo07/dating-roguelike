b15a3a91cc3a7c547f305b7eb27031a660fdd4e2
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-09-09 12:27:33 -0400

    move rooms code

---
+++ Assets/Scripts/Rooms/MoveRoomA.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+
+public class MoveRoomA : MonoBehaviour
+{
+    [Header("Move")]
+    public float moveSpeed;
+    public float moveDistance;
+    public bool move;
+    private bool isMoving;
+    public bool moveForwardDistance;
+    private Vector3 targetMovePosition;
+
+    [Header("Teleport Room")]
+    public Vector3 targetSpawnPosition;
+    public Vector3 targetTeleportPosition;
+    public float teleportDistanceThreshold;
+    private bool teleported;
+
+    BattleSystem battleSystem;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //battleSystem = GameObject.FindWithTag("BSystem").GetComponent<BattleSystem>();
+
+        move = false;
+        isMoving = false;
+        teleported = false;
+
+        targetSpawnPosition = new Vector3(0f, 0f, 0f);
+        targetTeleportPosition = new Vector3(0f, 0f, 39f);
+        teleportDistanceThreshold = 0.01f;
+        moveSpeed = 15f;
+    }
+
+    private void Update()
+    {
+        //if (battleSystem.state == BattleState.WON && !isMoving)
+        if (move && !isMoving)
+        {
+            moveDistance = moveForwardDistance ? 1f : 38f;
+            moveForwardDistance = !moveForwardDistance;
+
+            targetMovePosition = transform.position + transform.forward * moveDistance;
+            isMoving = true;
+            move = false;
+            teleported = false;
+        }
+
+        // Handle movement
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetMovePosition, moveSpeed * Time.deltaTime);
+
+            // When reached targetTeleportLocation
+            if (Vector3.Distance(transform.position, targetTeleportPosition) <= teleportDistanceThreshold)
+            {
+                if (!teleported)
+                {
+                    transform.position = targetSpawnPosition;
+                    teleported = true;
+                }
+
+                isMoving = false;
+            }
+            else if (Vector3.Distance(transform.position, targetMovePosition) <= teleportDistanceThreshold)
+            {
+                isMoving = false;
+            }
+        }
+    }
+}
