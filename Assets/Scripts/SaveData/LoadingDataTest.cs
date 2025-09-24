bdffccda7ea6d135383f4196dccd1c6c2b307fd8
Matthew Yang <matthewman216@gmail.com>
2025-09-24 17:08:18 -0400

    CoinSystem Add and Buy Item

--- Assets/Scripts/SaveData/LoadingDataTest.cs
+++ Assets/Scripts/SaveData/LoadingDataTest.cs
@@ -7,8 +7,6 @@ using UnityEngine.InputSystem;
 
 public class LoadingDataTest : MonoBehaviour, IDataPersistence
 {
-    public TextMeshProUGUI testText;
-    private int testCounter;
     private SimpleHealth player, enemy;
 
     void Awake()
@@ -17,23 +15,17 @@ public class LoadingDataTest : MonoBehaviour, IDataPersistence
         player = GameObject.FindWithTag("Player").GetComponent<SimpleHealth>();
     }
 
-    public void testCount(InputAction.CallbackContext context)
-    {
-        testCounter++;
-        testText.SetText("testCounter =" + testCounter);
-    }
-
     //Take the game data and load the needed variables into the script
     public void LoadData(GameData data)
     {
-        this.testCounter = data.testCounter;
+        //this.testCounter = data.testCounter;
 
     }
 
     //take the current value of the variables and save their values to the game data
     public void SaveData(ref GameData data)
     {
-        data.testCounter = this.testCounter;
+        //data.testCounter = this.testCounter;
     }
 
     public void TestTakeDamage()
--- Assets/Scripts/Shop/CoinSystem.cs
+++ Assets/Scripts/Shop/CoinSystem.cs
@@ -1,31 +1,77 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
-public class CoinSystem : MonoBehaviour
+public class CoinSystem : MonoBehaviour, IDataPersistence
 {
     public int coins;
-    bool itemPurchased;
+    [SerializeField] BattleSystem battleSystem;
+    [SerializeField] private TextMeshProUGUI coinUI;
+    [SerializeField] private TextMeshProUGUI coinTotal;
+    [SerializeField] private bool banana;
 
     // Coins to be used for unlocking new cards and classes in the shop
 
     // Item buttons check cost, if coins > cost then subtract coins
 
-    //
-
-
-
-    void AddCoin()
+    #region Save and Load
+    public void SaveData(ref GameData data)
     {
-        coins += 1;
+        data.coins = this.coins;
     }
 
-    void SubCoin()
+    public void LoadData(GameData data)
     {
-        coins -= 1;
+        this.coins = data.coins;
+    }
+    #endregion
+
+    private void Awake()
+    {
+        coinTotal = GameObject.Find("TotalCoins").GetComponent<TextMeshProUGUI>();
+        coinUI = GameObject.Find("coinUI").GetComponent<TextMeshProUGUI>();
+        battleSystem = GameObject.FindWithTag("BSystem").GetComponent<BattleSystem>();
     }
 
-    public void BuyItem(int cost)
+    private void Start()
     {
-        coins -= cost;
+        coinTotal.SetText("Coins: " + coins);
+    }
+
+    private void Update()
+    {
+        if (battleSystem.state == BattleState.WON && banana == false)
+        {
+            Debug.LogWarning("Attempting to add coins...");
+            StartCoroutine(AddCoins(8));
+            banana = true;
+        }
+        else if (battleSystem.state != BattleState.WON)
+        {
+            Debug.LogWarning("Banana bool reverted");
+            banana = false;
+        }
+    }
+
+    public IEnumerator AddCoins(int addcoins)
+    {
+        coins += addcoins;
+        coinUI.SetText("+" + addcoins + " Coin");
+        coinTotal.SetText("Coins: " + coins);
+        yield return new WaitForSeconds(2f);
+        coinUI.SetText("");
+    }
+
+    public IEnumerator BuyItem(int cost)
+    {
+        if (coins > cost)
+        {
+            coins -= cost;
+            coinUI.SetText("-" + coins + " Coin");
+            coinTotal.SetText("Coins: " + coins);
+            yield return new WaitForSeconds(2f);
+        }
     }
 
 }
