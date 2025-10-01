d9526681806f978fdba4bdede0620d7c9fac055d
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-10-01 16:14:23 -0400

    added status effect

---
+++ Assets/Scripts/Enemies/StatusEffects.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public enum StatusEffect
+{
+    NORMAL, STUN, KEEPSHIELD, SHIELDIGNORED, VULNERABLE, IVENCIBLE
+}
+public class StatusEffects : MonoBehaviour
+{
+    public StatusEffect currentStatus;
+    
+}
