ad70873f55d3c2bfb256039322b1563654a9b361
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-10-06 16:03:19 -0400

    Revert "some show sprites"
    This reverts commit d229dd4f1aab7ceb6af8d747af98240f5ac08a4d.

--- Assets/Scripts/UI/CardUI.cs
+++ Assets/Scripts/UI/CardUI.cs
@@ -1,15 +1,12 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CardUI : MonoBehaviour
 {
     public TMP_Text cardNameText;
-    public Image cardSpriteImage;
 
-    public void Setup(string cardName, Sprite cardImage)
+    public void Setup(string cardName)
     {
         cardNameText.text = cardName;
-        cardSpriteImage.sprite = cardImage;
     }
 }
