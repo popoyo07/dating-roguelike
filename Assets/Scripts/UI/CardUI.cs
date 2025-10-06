f0838b76d1c80b0ae406c6e29ec7098dacf29397
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-10-06 16:53:25 -0400

    Visual Deck is now buttons

--- Assets/Scripts/UI/CardUI.cs
+++ Assets/Scripts/UI/CardUI.cs
@@ -1,17 +1,31 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class CardUI : MonoBehaviour
 {
+    [Header("UI References")]
     public TMP_Text cardNameText;
     public Image cardSpriteImage;
+    public Button cardButton;
+
+    private string cardName;
 
     public void Setup(string cardName, Sprite cardSprite)
     {
+        this.cardName = cardName;
         cardNameText.text = cardName;
 
         if (cardSpriteImage != null)
             cardSpriteImage.sprite = cardSprite;
+
+        if (cardButton != null)
+            cardButton.onClick.AddListener(OnButtonClick);
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.LogWarning($"Card clicked: {cardName}");
     }
 }
