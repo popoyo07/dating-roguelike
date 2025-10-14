11e9ec690e942af7265a6d753cdb2ff5ec57da7c
Sze <sz982600@ucf.edu>
2025-10-14 03:06:20 -0400

    enemy size wtf?

--- Assets/Scripts/Animation/Animation.cs
+++ Assets/Scripts/Animation/Animation.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Animation : MonoBehaviour
 {
     public Animator animator;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = this.gameObject.GetComponentInChildren<Animator>();
 
+
+    }
+
+    public void AnimationTrigger(StatusEffect attackerStatus)
+    {
+
+        switch (attackerStatus)
+        {
+            case StatusEffect.SHIELDIGNORED:
+                TriggerShaking();
+                break;
+            case StatusEffect.VULNERABLE: // increases the dmg received by 50%
+                //TriggerBounce();
+                break;
+            case StatusEffect.WEAK:
+                TriggerWeak();
+                break;
+            default:
+                TriggerBounce();
+                break;
+        }
 
     }
 
@@ -22,6 +46,17 @@ public class Animation : MonoBehaviour
     {
         animator.SetTrigger("Attack");
     }
+
+    public void TriggerShaking()
+    {
+        animator.SetTrigger("Shake");
+    }
+
+    public void TriggerWeak()
+    {
+        animator.SetTrigger("Weak");
+    }
+
 
     // Update is called once per frame
     void Update()
