36ce02cd52f82938352c434696546d50505a5523
SonicZach04 <soniczachary04@gmail.com>
2025-10-14 21:37:12 -0400

    StartedNewShader

---
+++ Assets/Shaders/New/CustomLighting.hlsl
@@ -0,0 +1,24 @@
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+struct CustomLightingData
+{
+    //Surface Attributes
+    float3 albedo;
+};
+
+float3 CalculateCustomLighting(CustomLightingData d)
+{
+    return d.albedo;
+}
+
+void CalculateCustomLighting_float(float3 Albedo, out float3 Color)
+{
+    CustomLightingData d;
+    d.albedo = Albedo;
+    
+    Color = CalculateCustomLighting(d);
+}
+
+#endif 
+
