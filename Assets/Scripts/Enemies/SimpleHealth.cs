a5280b59fdd79c6757b96488f9049089c926c7af
popoyo07 <piwedaa@gmail.com>
2025-10-27 15:09:10 -0400

    quick fix of healthbars

--- Assets/Scripts/Enemies/SimpleHealth.cs
+++ Assets/Scripts/Enemies/SimpleHealth.cs
@@ -83,7 +83,7 @@ public class SimpleHealth : MonoBehaviour, IDataPersistence
         healthAmount = healthBuff * 10;
         maxHealth += healthAmount;
         health = maxHealth;
-        healthBar.UpdateMaxHealth();
+        StartCoroutine( healthBar.UpdateMaxHealth());
         healthBar.UpdateHealth();
     }
     public void ReceiveDMG(int dmg, StatusEffect attackerStatus) 
--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 public class HealthBar : MonoBehaviour
 {
     private int Health;
@@ -9,23 +10,31 @@ public class HealthBar : MonoBehaviour
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         avatar = this.gameObject.GetComponent<SimpleHealth>();
         healthBar.maxValue = avatar.maxHealth;
         healthBar.value = avatar.health;
     }
 
-    public void UpdateHealth()
+    public void UpdateHealth() // this one will get component is avatar is null 
     {
+        if (avatar == null)
+        {
+            avatar = this.gameObject.GetComponent<SimpleHealth>();
+
+        }
         healthBar.value = avatar.health;
         Debug.Log(" Health bar value " + healthBar.value + " hp value " + avatar.health);
 
     }
 
-    public void UpdateMaxHealth()
+    public IEnumerator UpdateMaxHealth() // this will wait for avatar to be not null to run
     {
-        healthBar.maxValue = avatar.maxHealth;
+        yield return new WaitUntil(() => 
+        avatar.gameObject.GetComponent<SimpleHealth>() != null);
+
+       healthBar.maxValue = avatar.maxHealth;
         Debug.Log(healthBar.value + avatar.health);
     }
 }
