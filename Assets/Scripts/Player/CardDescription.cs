96bffffe6daf421c22df2ce3418f0e80aa77af8c
popoyo07 <piwedaa@gmail.com>
2025-10-29 11:54:16 -0400

    kind of fixed the text bug

--- Assets/Scripts/Player/CardDescription.cs
+++ Assets/Scripts/Player/CardDescription.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class CardDescription : MonoBehaviour
 {
     AssignCard assignCard;
-    TextMeshProUGUI txt;
+    public TextMeshProUGUI txt;
     DeckDraw deckDraw;
+    bool runing;
+    bool isItEnabled;
     void OnEnable()
     {
         if (assignCard == null)
@@ -20,7 +23,14 @@ public class CardDescription : MonoBehaviour
      
         StartCoroutine(SetText());
     }
-
+    void Update()
+    {
+        if (assignCard != null && assignCard.cardUsed)
+        {
+            StartCoroutine(CardUsed());
+        }
+      
+    }
     IEnumerator SetText()
     {
         yield return new WaitUntil(() => 
@@ -30,5 +40,18 @@ public class CardDescription : MonoBehaviour
     
         txt.text = deckDraw.allCardDescriptions[assignCard.cardNameFromList];
 
+    }
+   IEnumerator CardUsed()
+    {
+        if (runing)
+        {
+            yield break;
+        }
+        runing = true;
+        yield return new WaitForSeconds(1);
+       
+        runing = false;
+        isItEnabled = false;
+        txt.gameObject.SetActive(isItEnabled);
     }
 }
